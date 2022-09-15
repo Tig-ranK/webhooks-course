82819664b989b3a0e9e036c796a0551b858e9c0f
Tigran Karakhanyan <tkarakhanyan@webmd.net>
2022-09-15 14:17:41 +0400

    Changed Express Discorder to use async/await instead of Promise API

--- code/express-discorder/server.js
+++ code/express-discorder/server.js
@@ -7,7 +7,8 @@ const port = 3000;
 
 app.use(express.json());
 
-app.get("/", (req, res) => res.send(`
+app.get("/", (req, res) =>
+   res.send(`
   <html>
     <head><title>Success!</title></head>
     <body>
@@ -15,36 +16,39 @@ app.get("/", (req, res) => res.send(`
       <img src="https://media.giphy.com/media/XreQmk7ETCak0/giphy.gif" alt="Cool kid doing thumbs up" />
     </body>
   </html>
-`));
+`)
+);
 
-app.post("/github", (req, res) => {
-  const content = ":wave: Hi mom!";
-  const avatarUrl = "https://media.giphy.com/media/3o7TKSjRrfIPjeiVyM/giphy.gif";
-  axios
-    .post(process.env.DISCORD_WEBHOOK_URL, {
-      content: content,
-      embeds: [
-        {
-          image: {
-            url: avatarUrl,
-          },
-        },
-      ],
-    })
-    .then((discordResponse) => {
+app.post("/github", async (req, res) => {
+   try {
+      const username = req.body.sender.login;
+      const repo = req.body.repository.full_name;
+      const content = `${username} just starred ${repo} ⭐`;
+      await axios.post(process.env.DISCORD_WEBHOOK_URL, {
+         content: content,
+         embeds: [
+            {
+               image: {
+                  url: req.body.sender.avatar_url,
+               },
+            },
+         ],
+      });
+
       console.log("Success!");
       res.status(204).send();
-    })
-    .catch((err) => console.error(`Error sending to Discord: ${err}`));
+   } catch (err) {
+      console.error(`Error sending to Discord: ${err}`);
+   }
 });
 
 app.use((error, req, res, next) => {
-  res.status(500)
-  res.send({error: error})
-  console.error(error.stack)
-  next(error)
-})
+   res.status(500);
+   res.send({ error: error });
+   console.error(error.stack);
+   next(error);
+});
 
 app.listen(port, () =>
-  console.log(`Example app listening at http://localhost:${port}`)
+   console.log(`Example app listening at http://localhost:${port}`)
 );
